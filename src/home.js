92ca55716c8c5d86d9ea49ca07c03f4e344d8a7a
tennjugu <133909875+tennjugu@users.noreply.github.com>
2023-09-04 16:20:59 +0100

    add homepage content

--- src/home.js
+++ src/home.js
@@ -8,17 +8,26 @@ const homePage = () =>{
 
 
     const grid = document.createElement('div')
-    grid.classList.add('grid')
+    grid.classList.add  ('grid')
+
+
     const RestImage = document.createElement('img')
     RestImage.src = '../src/jason-leung-poI7DelFiVA-unsplash.jpg'
     RestImage.alt = 'restaurant image'
+
+
+    const captionDiv = document.createElement('div')
+    captionDiv.classList.add ('captionDiv')
     const caption = document.createElement('h3')
-    caption.classList.add = 'caption'
+    caption.classList.add ( 'caption')
     caption.textContent = `At Bigg Bites Restaurant, we take pride in offering a remarkable dining experience that combines exquisite flavors, exceptional service, and an inviting atmosphere. Our expert chefs craft each dish with passion and creativity, using only the finest ingredients. Whether you're joining us for a special occasion or a casual meal, we promise an unforgettable culinary journey that will tantalize your taste buds and leave you craving for more.`
 
     page.appendChild(heading)
     grid.appendChild(RestImage)
-    grid.appendChild(caption)
+
+    captionDiv.appendChild(caption)
+    grid.appendChild(captionDiv)
+
     page.appendChild(grid)
     content.appendChild(page)
 }
