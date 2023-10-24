6906740fcaa6e01eb2069f85919e4c3c76724aa1
Jugu Tenerat <133909875+tennjugu@users.noreply.github.com>
2023-10-24 22:04:11 +0100

    add close hamburger to clicked area outside nav links

--- src/index.js
+++ src/index.js
@@ -3,19 +3,30 @@ load()
 
 const hamburger = document.querySelector(".hamburger")
 const navMenu = document.querySelector(".navItems")
-const pageContent = document.querySelector(".page")
 const navLinks = document.querySelectorAll(".nav-menu")
+const pageContent = document.querySelector(".page")
 hamburger.addEventListener("click", () =>{
-    pageContent.classList.toggle("active")
     hamburger.classList.toggle("active")
     navMenu.classList.toggle("active")
+    pageContent.classList.toggle("active")
 })
 
 
-navLinks.forEach(menu => 
-    menu.addEventListener("click", () =>{
-        hamburger.classList.remove("active")
-        navMenu.classList.remove("active")
-        pageContent.classList.remove("active")
-    })
-)
+navLinks.forEach((menu) => 
+    menu.addEventListener("click", closeMenu
+))
+
+window.addEventListener("click", (event) => {
+    if(
+        !event.target.closest(".hamburger") &&
+        !event.target.closest(".navItems")
+    ) {
+        closeMenu()
+    }
+})
+
+function closeMenu() {
+    hamburger.classList.remove("active")
+    navMenu.classList.remove("active")
+    pageContent.classList.remove("active")
+}
